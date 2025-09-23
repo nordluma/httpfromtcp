c062f6b18ad899cdbd223b5c8001bd9146b6026d
nordluma <maxnordlund.95@hotmail.com>
2025-09-23 17:54:33 +0300

    feat(main): read `messages.txt` in 8 byte chunks

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Printf("error opening file: %v\n", err)
+	}
+	defer file.Close()
+
+	for {
+		buf := make([]byte, 8)
+		if _, err = file.Read(buf); err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+
+		fmt.Printf("read: %s\n", buf)
+	}
 }
