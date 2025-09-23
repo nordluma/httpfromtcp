7a51597f94b0af8e00bf4fa2e0d7d9d72fd4c68f
nordluma <maxnordlund.95@hotmail.com>
2025-09-24 00:13:46 +0300

    refactor(cmd/tcplistener): move tcp listener logic

--- main.go
+++ main.go
@@ -1,69 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net"
-	"strings"
-)
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	strCh := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(strCh)
-
-		currLine := ""
-		for {
-			buf := make([]byte, 8)
-			n, err := f.Read(buf)
-			if err != nil {
-				if currLine != "" {
-					fmt.Printf("read: %s\n", currLine)
-					currLine = ""
-				}
-
-				if err == io.EOF {
-					break
-				}
-
-				fmt.Printf("error reading chunk: %s\n", err.Error())
-				break
-			}
-
-			str := string(buf[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				strCh <- fmt.Sprintf("%s%s", currLine, parts[i])
-				currLine = ""
-			}
-			currLine += parts[len(parts)-1]
-
-		}
-	}()
-
-	return strCh
-}
+import "fmt"
 
 func main() {
-	listener, err := net.Listen("tcp", "127.0.0.1:42069")
-	if err != nil {
-		fmt.Printf("error starting tcp listener: %s\n", err.Error())
-		return
-	}
-	defer listener.Close()
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Printf("error accepting new connection: %s\n", err.Error())
-			continue
-		}
-
-		strCh := getLinesChannel(conn)
-		for line := range strCh {
-			fmt.Printf("read: %s\n", string(line))
-		}
-	}
+	fmt.Println("Something is happening soon")
 }
