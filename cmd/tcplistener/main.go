de9f5e5bf125e90ef3f8e0014ec53dc1da6a59aa
nordluma <maxnordlund.95@hotmail.com>
2025-09-24 17:49:48 +0300

    feat(cmd/tcplistener): parse and print request

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,49 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"strings"
+
+	"github.com/nordluma/httpfromtcp/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	strCh := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(strCh)
-
-		currLine := ""
-		for {
-			buf := make([]byte, 8)
-			n, err := f.Read(buf)
-			if err != nil {
-				if currLine != "" {
-					fmt.Printf("read: %s\n", currLine)
-					currLine = ""
-				}
-
-				if err == io.EOF {
-					break
-				}
-
-				fmt.Printf("error reading chunk: %s\n", err.Error())
-				break
-			}
-
-			str := string(buf[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				strCh <- fmt.Sprintf("%s%s", currLine, parts[i])
-				currLine = ""
-			}
-			currLine += parts[len(parts)-1]
-
-		}
-	}()
-
-	return strCh
-}
 
 func main() {
 	listener, err := net.Listen("tcp", "127.0.0.1:42069")
@@ -62,9 +23,17 @@ func main() {
 		}
 		fmt.Printf("Connection accepted: %s\n", conn.RemoteAddr())
 
-		strCh := getLinesChannel(conn)
-		for line := range strCh {
-			fmt.Printf("read: %s\n", string(line))
+		request, err := request.RequestFromReader(conn)
+		if err != nil {
+			fmt.Printf("error parsing request: %s", err.Error())
+			continue
 		}
+
+		fmt.Printf(
+			"Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
+			request.RequestLine.Method,
+			request.RequestLine.RequestTarget,
+			request.RequestLine.HttpVersion,
+		)
 	}
 }
