be767dc99df791cebd9b70d3ae108e74449db52a
nordluma <maxnordlund.95@hotmail.com>
2025-09-24 11:56:56 +0300

    test(internal/request): parses request line correctly

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,75 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+func TestParsesRequestLine(t *testing.T) {
+	r, err := RequestFromReader(
+		strings.NewReader(
+			"GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		),
+	)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestParseRequestLineWithPath(t *testing.T) {
+	r, err := RequestFromReader(
+		strings.NewReader(
+			"GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81\r\nAccept: */*\r\n\r\n",
+		),
+	)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestParsePostRequestLineWithPath(t *testing.T) {
+	r, err := RequestFromReader(
+		strings.NewReader(
+			"POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81\r\nAccept: */*\r\n\r\n",
+		),
+	)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestInvalidNumberOfRequestLineParts(t *testing.T) {
+	_, err := RequestFromReader(
+		strings.NewReader(
+			"/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81\r\nAccept: */*\r\n\r\n",
+		),
+	)
+	require.Error(t, err)
+}
+
+func TestInvalidMethodOrder(t *testing.T) {
+	_, err := RequestFromReader(
+		strings.NewReader(
+			"/ GET HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		),
+	)
+	require.Error(t, err)
+}
+
+func TestInvalidHttpVersion(t *testing.T) {
+	_, err := RequestFromReader(
+		strings.NewReader(
+			"/ GET HTTP/69.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		),
+	)
+	require.Error(t, err)
 }
